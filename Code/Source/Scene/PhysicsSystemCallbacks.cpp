8e3a0b5c59b7286cddb46e0eabe8b066390de9d2
iamjbq <iamjbq@gmail.com>
2025-10-02 13:21:06 -0300

    Initial contact data for OnContact
    Finish contact point data

--- Code/Source/Scene/PhysicsSystemCallbacks.cpp
+++ Code/Source/Scene/PhysicsSystemCallbacks.cpp
@@ -9,7 +9,8 @@
 
 namespace JoltPhysics
 {
-    static constexpr const JPH::uint32 MaxPointsToReport = 10;
+    // PhysX set to 10, Jolt provides up to 64 points
+    static constexpr const JPH::uint32 MaxPointsToReport = 64;
     
     // JoltContactListener TODO: Complete these functions
     
@@ -22,8 +23,8 @@ namespace JoltPhysics
         return JPH::ValidateResult::AcceptAllContactsForThisBodyPair;
     }
 
-    void JoltContactListener::OnContactAdded([[maybe_unused]] const JPH::Body& inBody1, [[maybe_unused]] const JPH::Body& inBody2,
-        [[maybe_unused]] const JPH::ContactManifold& inManifold, [[maybe_unused]] JPH::ContactSettings& ioSettings)
+    void JoltContactListener::OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2,
+        const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings)
     {
         // Collision Event
         AzPhysics::CollisionEvent collision;
@@ -38,25 +39,30 @@ namespace JoltPhysics
         /// When contacts are added, the constraint solver has not run yet, so the collision impulse is unknown at that point.
         /// The velocities of inBody1 and inBody2 are the velocities before the contact has been resolved, so you can use this to
         /// estimate the collision impulse to e.g. determine the volume of the impact sound to play (see: EstimateCollisionResponse).
-
         JPH::CollisionEstimationResult estimate;
-        JPH::EstimateCollisionResponse(inBody1, inBody2, inManifold, estimate, ioSettings.mCombinedFriction, ioSettings.mCombinedRestitution);
+        JPH::EstimateCollisionResponse(inBody1, inBody2, inManifold, estimate, ioSettings.mCombinedFriction, ioSettings.mCombinedRestitution, 1.0f, 10);
 
         // Extract contacts for collision event
         // TODO: Need to sort out penetration depth != 0 where contact points on 1 != 2
-        JPH::uint contactPointCount = inManifold.mRelativeContactPointsOn1.size();
-        // collision.m_contacts.resize(contactPointCount <= MaxPointsToReport? contactPointCount : MaxPointsToReport); // should limit to <= MaxPointsToReport
-        collision.m_contacts.resize(contactPointCount);
-        for (JPH::uint i = 0; i < contactPointCount; ++i)
+        JPH::uint contactPointCount = AZStd::GetMax(inManifold.mRelativeContactPointsOn1.size(), inManifold.mRelativeContactPointsOn2.size()) ;
+        JPH::uint pointsToReport = contactPointCount <= MaxPointsToReport ? contactPointCount : MaxPointsToReport;
+        collision.m_contacts.resize(pointsToReport);
+        for (JPH::uint i = 0; i < pointsToReport; ++i)
         {
-            const JPH::Vec3 point = inManifold.GetWorldSpaceContactPointOn1(i);
+            const JPH::Vec3 point1 = inManifold.GetWorldSpaceContactPointOn1(i);
+            const JPH::Vec3 point2 = inManifold.GetWorldSpaceContactPointOn2(i);
+
+            // Construct the impulse vector from constituents
+            auto [mContactImpulse, mFrictionImpulse1, mFrictionImpulse2] = estimate.mImpulses.at(i);
+            const JPH::Vec3 impulseVector = mContactImpulse * inManifold.mWorldSpaceNormal +
+                                            mFrictionImpulse1 * estimate.mTangent1 +
+                                            mFrictionImpulse2 * estimate.mTangent2;
             
             AzPhysics::Contact& contact = collision.m_contacts[i];
-            
-            contact.m_position = JoltMathConvert(point);
+            contact.m_position = JoltMathConvert(point1);
             contact.m_normal = JoltMathConvert(inManifold.mWorldSpaceNormal);
-            contact.m_impulse = JoltMathConvert(estimate.mImpulses.at(i).mContactImpulse * inManifold.mWorldSpaceNormal);
-            contact.m_separation = inManifold.mPenetrationDepth;
+            contact.m_impulse = JoltMathConvert(impulseVector);
+            contact.m_separation = inManifold.mPenetrationDepth; // TODO: Probably should take distance between the two points
             // contact.m_internalFaceIndex01 = ;
             // contact.m_internalFaceIndex02 = ;
         }
