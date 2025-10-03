7a73377ed715781590330417b7b7b5ddcc9751b7
iamjbq <iamjbq@gmail.com>
2025-10-02 22:51:29 -0300

    Changed to AZ_FORCE_INLINE to follow convention

--- Code/Include/JoltPhysics/MathConversions.h
+++ Code/Include/JoltPhysics/MathConversions.h
@@ -10,38 +10,38 @@
 // #include <Jolt/Math/Vec3.h>
 #include <Jolt/Geometry/AABox.h>
 
-inline JPH::Vec3 JoltMathConvert(const AZ::Vector3& inVec)
+AZ_FORCE_INLINE JPH::Vec3 JoltMathConvert(const AZ::Vector3& inVec)
 {
     return JPH::Vec3(inVec.GetX(), inVec.GetY(), inVec.GetZ());
 }
 
-inline AZ::Vector3 JoltMathConvert(const JPH::Vec3& inVec)
+AZ_FORCE_INLINE AZ::Vector3 JoltMathConvert(const JPH::Vec3& inVec)
 {
     return AZ::Vector3(inVec.GetX(), inVec.GetY(), inVec.GetZ());
 }
 
-inline AZ::Vector4 JoltMathConvert(const JPH::Vec4& inVec)
+AZ_FORCE_INLINE AZ::Vector4 JoltMathConvert(const JPH::Vec4& inVec)
 {
     return AZ::Vector4(inVec.GetX(), inVec.GetY(), inVec.GetZ(), inVec.GetW());
 }
 
-inline JPH::Vec4 JoltMathConvert(const AZ::Vector4& inVec)
+AZ_FORCE_INLINE JPH::Vec4 JoltMathConvert(const AZ::Vector4& inVec)
 {
     return JPH::Vec4(inVec.GetX(), inVec.GetY(), inVec.GetZ(), inVec.GetW());
 }
 
-inline JPH::Quat JoltMathConvert(const AZ::Quaternion& inQuat)
+AZ_FORCE_INLINE JPH::Quat JoltMathConvert(const AZ::Quaternion& inQuat)
 {
     return JPH::Quat(inQuat.GetX(), inQuat.GetY(), inQuat.GetZ(), inQuat.GetW());
 }
 
-inline AZ::Quaternion JoltMathConvert(const JPH::Quat& inQuat)
+AZ_FORCE_INLINE AZ::Quaternion JoltMathConvert(const JPH::Quat& inQuat)
 {
     return AZ::Quaternion(inQuat.GetX(), inQuat.GetY(), inQuat.GetZ(), inQuat.GetW());
 }
 
 // Why is there only Jolt->AZ direction for this one?
-inline AZ::Aabb JoltMathConvert(const JPH::AABox& bounds)
+AZ_FORCE_INLINE AZ::Aabb JoltMathConvert(const JPH::AABox& bounds)
 {
     // check that the Jolt bounds are valid, otherwise CreateFromMinMax will assert.
     if (bounds.IsValid())
@@ -51,7 +51,7 @@ inline AZ::Aabb JoltMathConvert(const JPH::AABox& bounds)
     return AZ::Aabb::CreateNull();
 }
 
-// inline JPH::Mat44 JoltMathConvert(const AZ::Transform& lyTransform)
+// AZ_FORCE_INLINE JPH::Mat44 JoltMathConvert(const AZ::Transform& lyTransform)
 // {
 //     AZ::Quaternion quat = lyTransform.GetRotation();
 //     AZ::Vector3 vec3 = lyTransform.GetTranslation();
@@ -60,7 +60,7 @@ inline AZ::Aabb JoltMathConvert(const JPH::AABox& bounds)
 //         JoltMathConvert(quat).getNormalized());
 // }
 //
-// inline AZ::Transform JoltMathConvert(const physx::PxTransform& pxTransform)
+// AZ_FORCE_INLINE AZ::Transform JoltMathConvert(const physx::PxTransform& pxTransform)
 // {
 //     return AZ::Transform::CreateFromQuaternionAndTranslation(JoltMathConvert(pxTransform.q), JoltMathConvert(pxTransform.p));
 // }
