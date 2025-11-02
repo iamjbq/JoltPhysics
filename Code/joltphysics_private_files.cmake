3376c133a4b3c200e42bf18f117f007f1eabbd25
iamjbq <iamjbq@gmail.com>
2025-11-02 06:54:23 -0400

    Moved API files to private files to align with PhysX

--- Code/joltphysics_private_files.cmake
+++ Code/joltphysics_private_files.cmake
@@ -1,5 +1,21 @@
 
 set(FILES
+		Include/JoltPhysics/JoltPhysicsBus.h
+		Include/JoltPhysics/JoltPhysicsTypeIds.h
+		Include/JoltPhysics/JoltRigidBodyInterface.h
+		Include/JoltPhysics/JoltPhysicsTypes.h
+		Include/JoltPhysics/MathConversions.h
+		Include/JoltPhysics/Configuration/JoltConfiguration.h
+		Include/JoltPhysics/Material/JoltMaterial.h
+		Include/JoltPhysics/Material/JoltMaterialConfiguration.h
+		Include/JoltPhysics/BodyData.h
+		Include/JoltPhysics/BodyData.inl
+		Include/JoltPhysics/Utils.h
+		Include/JoltPhysics/Utils.inl
+		Include/JoltPhysics/ColliderComponentBus.h
+		Include/JoltPhysics/ColliderShapeBus.h
+		Include/JoltPhysics/EditorColliderComponentRequestBus.h
+
         Source/JoltPhysicsModuleInterface.cpp
         Source/JoltPhysicsModuleInterface.h
 
