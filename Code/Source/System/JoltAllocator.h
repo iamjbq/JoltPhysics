4d22b7f5c84416e81d222894193c9d38b78f39fd
Jason Q <119447878+iamjbq@users.noreply.github.com>
2026-02-14 22:19:55 -0400

    Hacky implementation of a static buffer allocator
    I don't think we can use this since it needs to know size at compile time in the template params.

--- Code/Source/System/JoltAllocator.h
+++ Code/Source/System/JoltAllocator.h
@@ -3,24 +3,31 @@
 #include <Jolt/Jolt.h>
 #include <Jolt/Core/TempAllocator.h>
 
-#include <AzCore/Memory/ChildAllocatorSchema.h>
-#include <AzCore/Memory/SystemAllocator.h>
-
 namespace JoltPhysics
 {
-    //! System allocator to be used for all .
-    AZ_CHILD_ALLOCATOR_WITH_NAME(JoltAllocator, "JoltAllocator", "{3B69EBB9-A4A3-4E3A-97D2-17B3536D6578}", AZ::SystemAllocator);
-
-    //! Implementation of the Jolt allocator interface using Open 3D Engine allocator.
+    //! Implementation of the Jolt allocator interface using a static_buffer_allocator
+    template<AZStd::size_t Size>
     class JoltAzAllocatorCallback
         : public JPH::TempAllocator
     {
+        static_assert(Size > 0, "Size of static buffer must be not be 0");
+        typedef JoltAzAllocatorCallback<Size> this_type;
     public:
         ~JoltAzAllocatorCallback() override = default;
-        void* Allocate(JPH::uint inSize) override;
-        void Free(void* inAddress, JPH::uint inSize) override;
+
+        AZ_FORCE_INLINE void* Allocate(JPH::uint inSize) override
+        {
+            void* ptr = m_tempAllocator.allocate(inSize);
+            AZ_Assert((reinterpret_cast<size_t>(ptr) & 15) == 0, "Jolt requires 16-byte aligned memory allocations.");
+            return ptr;
+        }
+
+        AZ_FORCE_INLINE void Free(void* inAddress, JPH::uint inSize) override
+        {
+            m_tempAllocator.deallocate(inAddress, inSize);
+        }
 
     private:
-
+        AZStd::static_buffer_allocator<Size, 16> m_tempAllocator;
     };
 }
